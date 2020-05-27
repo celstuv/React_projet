c1bb7da963d981e685e35cc2f6526629d2713449
celine GA <celstuv@yahoo.fr>
2020-05-28 01:02:05 +0200

    DOM Events et changement de State : Changement paramètre via Bind, Paramètre et Input

--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -3,12 +3,12 @@ import Car from './Cars';
 
 class Mycars extends Component {
 
-  noCopy = () => { /*Fonction fléchée*/
-    alert('Merci de ne pas copier le texte');
-  }
-  addStyle = (e) => {
-    // console.log(e.target);
-      e.target.classList.toggle('styled')
+  // noCopy = () => { /*Fonction fléchée*/
+  //   alert('Merci de ne pas copier le texte');
+  // }
+  // addStyle = (e) => {
+  //   // console.log(e.target);
+  //     e.target.classList.toggle('styled')
       /*
       Deuxième méthode
       if (e.target.classList.contains('styled') ) {
@@ -16,17 +16,15 @@ class Mycars extends Component {
       } else {
           e.target.classList.add('styled');
       }
-      */
-
-}
+}  */
 
   render() {
     // console.log(this);
     /* console.log affiche props: {title: "Mon Catalogue de Voitures"}*/
     return(
       <div>
-        <h1 onMouseOver={this.addStyle}> {this.props.title}</h1>
-        <p onCopy={this.noCopy}> Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        <h1 /*onMouseOver={this.addStyle}*/> {this.props.title}</h1>
+        <p /*onCopy={this.noCopy}*/> Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
 
         <Car color="red">Ford</Car>
         {/* Car est le props , Ford est l'objet*/}
